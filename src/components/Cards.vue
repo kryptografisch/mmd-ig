52126d9b70aeed79356185842baec4259cd7566e
kryptografisch <code@kryptografische.biz>
2025-04-05 14:35:01 +0200

    added loading and error states to cards wrapper

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -4,6 +4,7 @@ import { ref } from "vue";
 import { ImageData, getImageData } from "../service/api.service";
 
 const images = ref<ImageData[]>([]);
+const api_error = ref<string | null>(null);
 
 onMounted(async () => {
   try {
@@ -11,13 +12,18 @@ onMounted(async () => {
     console.log(`Loaded ${data.length} images`);
     images.value = data;
   } catch (error) {
-    console.error(error);
+    api_error.value = error as string;
   }
 });
 </script>
 
 <template>
   <div class="cards">
+    <div v-if="images.length === 0 && !api_error">Loading...</div>
+    <div v-if="api_error !== null">
+      An error occurred: <code>{{ api_error }}</code
+      ><br />Please try reloading.
+    </div>
     <div v-for="image in images" :key="image.id">
       {{ image.id }}
       {{ image.description }}
--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -8,7 +8,13 @@ export interface ImageData {
 const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export const getImageData = async (): Promise<ImageData[]> => {
-  const response = await fetch(`${API_BASE}/getimages.php`);
-  const data = await response.json();
-  return data as ImageData[];
+  try {
+    const response = await fetch(`${API_BASE}/getimages.php`);
+    const data = await response.json();
+    return data as ImageData[];
+  } catch (error) {
+    console.log(`an error occurred on getImages`, error);
+    // TODO: map to error codes or messages using api response data or error object
+    throw "failed to fetch";
+  }
 };
